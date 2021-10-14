e56ce328701a13d47eeab46d308df0295182c395
Cintia Yamamoto <cintiafumi@gmail.com>
2021-10-13 23:37:05 -0300

    ✅  tests(list): add assertions

--- cypress/integration/list.spec.js
+++ cypress/integration/list.spec.js
@@ -8,13 +8,16 @@ describe('List', () => {
     })
     
     cy.visit('https://form-agilizei.netlify.app/listagem.html');
-  });
 
+    cy.get('table tbody tr').should('have.length', 0);
+  });
+  
   it('should list all records when there are registers', () => {
     cy.fixture('list-with-items').then((data) => {
       window.localStorage.setItem('data', JSON.stringify(data));
     })
-
+    
     cy.visit('https://form-agilizei.netlify.app/listagem.html');
+    cy.get('table tbody tr').should('have.length', 2);
   });
 });
